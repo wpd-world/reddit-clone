8ace3296d42e7099019a8370e3cf11a15c584d07
server <root@ubuntu-s-1vcpu-1gb-nyc1-01>
2019-03-22 14:52:11 +0000

    Removed details from database.js

--- configs/database.js
+++ configs/database.js
@@ -1,5 +1,5 @@
 module.exports.config = {
-    uri: 'mongodb+srv://admin:anime@cluster0-e7lsm.mongodb.net/test?retryWrites=true',
+    uri: '',
     options: {
         dbName: 'reddit-app',
         useNewUrlParser: true
